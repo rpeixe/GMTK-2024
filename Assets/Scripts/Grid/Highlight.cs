77256a319763d7c83840e8cd68ec24734ebf8cb7
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-08-18 02:48:26 -0300

    Created highlight effect

---
+++ Assets/Scripts/Grid/Highlight.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Tilemaps;
+
+public class Highlight : MonoBehaviour
+{
+    [SerializeField] TileBase _highlitTile;
+
+    private bool _tracking = false;
+    private Vector2Int previousMousePos = new Vector2Int(-1, -1);
+
+    private void OnMouseEnter()
+    {
+        _tracking = true;
+    }
+
+    private void OnMouseExit()
+    {
+        LevelManager.Instance.GridController.SetTarget(previousMousePos, null);
+        previousMousePos = new Vector2Int(-1, -1);
+        _tracking = false;
+    }
+
+    private void Update()
+    {
+        if (_tracking && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Vector2Int mousePos  = LevelManager.Instance.GridController.GetTilePos((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            if (mousePos != previousMousePos)
+            {
+                LevelManager.Instance.GridController.SetTarget(previousMousePos, null);
+                LevelManager.Instance.GridController.SetTarget(mousePos, _highlitTile);
+                previousMousePos = mousePos;
+            }
+        }
+    }
+}
