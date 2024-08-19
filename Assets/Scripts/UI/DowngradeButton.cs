6fd8ef76906b456cede9a8768df6f2c59ff3abea
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-08-19 20:14:36 -0300

    Added descriptions for upgrade and downgrade buttons

--- Assets/Scripts/UI/DowngradeButton.cs
+++ Assets/Scripts/UI/DowngradeButton.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DowngradeButton : MonoBehaviour
+public class DowngradeButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] TextMeshProUGUI _priceText;
     private Button _button;
+    private BuildingInformation _buildingInformation;
 
     private void Start()
     {
@@ -23,16 +25,16 @@ public class DowngradeButton : MonoBehaviour
 
     private void UpdateCosts()
     {
-        BuildingInformation buildingInformation = LevelManager.Instance.Selected.ConstructedBuilding.BuildingInformation;
+        _buildingInformation = LevelManager.Instance.Selected.ConstructedBuilding.BuildingInformation;
 
-        if (buildingInformation.Previous == null)
+        if (_buildingInformation.Previous == null)
         {
             _button.interactable = false;
             _priceText.text = $"";
             return;
         }
 
-        float cost = LevelManager.Instance.CalculateCost(1, LevelManager.Instance.Selected, buildingInformation.Previous)/2;
+        float cost = LevelManager.Instance.CalculateCost(1, LevelManager.Instance.Selected, _buildingInformation.Previous)/2;
         _priceText.text = $"$ {cost:00.00}";
 
         if (LevelManager.Instance.Selected.ConstructedBuilding.Deactivated)
@@ -49,5 +51,18 @@ public class DowngradeButton : MonoBehaviour
     {
         LevelManager.Instance.DowngradeBuilding(LevelManager.Instance.Selected);
         UIManager.Instance.Unselect();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_buildingInformation.Previous)
+        {
+            UIManager.Instance.ShowBuildingInfo(_buildingInformation.Previous);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        UIManager.Instance.HideBuildingInfo();
     }
 }
