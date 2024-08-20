f89862de3ddd6d27102cc8d6e6e1d1c936d610ce
gabrielc231 <142500547+gabrielc231@users.noreply.github.com>
2024-08-20 12:21:34 -0300

    Dialogue Implemented!

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public GameObject portrait;
+    public Sprite[] close;
+    public Sprite[] open;
+    public (int, string)[] lines { get; set; }
+    public float textSpeed = 0.1f;
+    public Image portraitImage;
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void InitDialog()
+    {
+        portraitImage = portrait.GetComponent<Image>();
+        textComponent.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index].Item2)
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index].Item2;
+            }
+        }
+    }
+    public void StartDialogue()
+    {
+        gameObject.SetActive(true);
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        var charArr = lines[index].Item2.ToCharArray();
+        foreach (char c in charArr)
+        {
+            if (c % 2 == 0 || c == charArr[charArr.Length-1])
+            {
+                portraitImage.sprite = close[lines[index].Item1];
+            }
+            else
+            {
+                portraitImage.sprite = open[lines[index].Item1];
+            }
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            portraitImage.sprite = close[lines[index].Item1];
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
--- Assets/Scripts/Initializers/Level1Initializer.cs
+++ Assets/Scripts/Initializers/Level1Initializer.cs
@@ -6,9 +6,21 @@ public class Level1Initializer : MonoBehaviour, ILevelInitializer
 {
     [SerializeField] BuildingInformation _hq;
     [SerializeField] BuildingInformation _monument;
-
+    [SerializeField] GameObject _dialogueBox;
+    private Dialogue d;
+    public void Dialogue()
+    {
+        d = _dialogueBox.GetComponent<Dialogue>();
+        d.InitDialog();
+        d.lines = new (int, string)[2];
+        d.lines[0] = (0, "Man, the company is dead. There is no way we can survive this market crash!");
+        d.lines[1] = (0, "Hold on a sec. Maybe I can do something on this Island.");
+        d.StartDialogue();
+        
+    }
     public void InitializeLevel()
     {
+        Dialogue();
         Building monument = LevelManager.Instance.ConstructBuilding(0, LevelManager.Instance.GridController.Cells[6,5], _monument, true, true);
         LevelManager.Instance.ConstructBuilding(1, LevelManager.Instance.GridController.Cells[12,3], _hq, true, true);
 
