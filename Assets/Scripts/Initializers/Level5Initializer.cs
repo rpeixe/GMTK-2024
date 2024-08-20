863117bce0377ae2b41ef6919656c23dce8edc79
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-08-20 11:55:15 -0300

    Fixed a bug where stage would not restart properly

--- Assets/Scripts/Initializers/Level5Initializer.cs
+++ Assets/Scripts/Initializers/Level5Initializer.cs
@@ -9,16 +9,16 @@ public class Level5Initializer : MonoBehaviour, ILevelInitializer
     [SerializeField] private BuildingInformation _hq;
     [SerializeField] private BuildingInformation _monument;
 
-    private Building _monument1;
-    private Building _monument2;
+    private int _player1Monuments = 0;
+    private int _player2Monuments = 0;
 
     public void InitializeLevel()
     {
         Building hq1 = LevelManager.Instance.ConstructBuilding(1, LevelManager.Instance.GridController.Cells[16,4], _hq, true, true);
         Building hq2 = LevelManager.Instance.ConstructBuilding(2, LevelManager.Instance.GridController.Cells[4,15], _hq, true, true);
 
-        _monument1 = LevelManager.Instance.ConstructBuilding(0, LevelManager.Instance.GridController.Cells[2, 7], _monument, true, true);
-        _monument2 = LevelManager.Instance.ConstructBuilding(0, LevelManager.Instance.GridController.Cells[12, 17], _monument, true, true);
+        Building monument1 = LevelManager.Instance.ConstructBuilding(0, LevelManager.Instance.GridController.Cells[2, 7], _monument, true, true);
+        Building monument2 = LevelManager.Instance.ConstructBuilding(0, LevelManager.Instance.GridController.Cells[12, 17], _monument, true, true);
 
         Building.OnBuildingCaptured += HandleBuildingCaptured;
 
@@ -31,12 +31,12 @@ public class Level5Initializer : MonoBehaviour, ILevelInitializer
 
         ai1.setPlayers(2);
         ai1.SetHQs(hqDicts);
-        ai1.SetGoals(new List<Building>() { _monument2, _monument1, hq1 });
+        ai1.SetGoals(new List<Building>() { monument2, monument1, hq1 });
 
         ai1.AddBuilding(hq1);
         ai1.AddBuilding(hq2);
-        ai1.AddBuilding(_monument1);
-        ai1.AddBuilding(_monument2);
+        ai1.AddBuilding(monument1);
+        ai1.AddBuilding(monument2);
 
         ai1.StartPlaying();
     }
@@ -54,11 +54,29 @@ public class Level5Initializer : MonoBehaviour, ILevelInitializer
 
     private void HandleMonumentCaptured(Building building, int oldOwner, int newOwner)
     {
-        if (_monument1.Owner == 1 && _monument2.Owner == 1)
+        if (oldOwner == 1)
+        {
+            _player1Monuments--;
+        }
+        else if (oldOwner == 2)
+        {
+            _player2Monuments--;
+        }
+
+        if (newOwner == 1)
+        {
+            _player1Monuments++;
+        }
+        else if (newOwner == 2)
+        {
+            _player2Monuments++;
+        }
+
+        if (_player1Monuments == 2)
         {
             LevelManager.Instance.Victory();
         }
-        else if (_monument1.Owner == 2 && _monument2.Owner == 2)
+        else if (_player2Monuments == 2)
         {
             LevelManager.Instance.Defeat();
         }
--- Assets/Scripts/Initializers/Level6Initializer.cs
+++ Assets/Scripts/Initializers/Level6Initializer.cs
@@ -10,15 +10,15 @@ public class Level6Initializer : MonoBehaviour, ILevelInitializer
     [SerializeField] private BuildingInformation _hq10;
     [SerializeField] private BuildingInformation _monument;
 
-    private Building _hq1;
-    private Building _hq2;
-    private Building _hq3;
+    private int _player1Hqs = 1;
+    private int _player2Hqs = 1;
+    private int _player3Hqs = 1;
 
     public void InitializeLevel()
     {
-        _hq1 = LevelManager.Instance.ConstructBuilding(1, LevelManager.Instance.GridController.Cells[13,1], _hq, true, true);
-        _hq2 = LevelManager.Instance.ConstructBuilding(2, LevelManager.Instance.GridController.Cells[17,17], _hq, true, true);
-        _hq3 = LevelManager.Instance.ConstructBuilding(3, LevelManager.Instance.GridController.Cells[1,13], _hq, true, true);
+        Building hq1 = LevelManager.Instance.ConstructBuilding(1, LevelManager.Instance.GridController.Cells[13, 1], _hq, true, true);
+        Building hq2 = LevelManager.Instance.ConstructBuilding(2, LevelManager.Instance.GridController.Cells[17, 17], _hq, true, true);
+        Building hq3 = LevelManager.Instance.ConstructBuilding(3, LevelManager.Instance.GridController.Cells[1, 13], _hq, true, true);
 
         Building monument = LevelManager.Instance.ConstructBuilding(0, LevelManager.Instance.GridController.Cells[10, 10], _monument, true, true);
 
@@ -26,30 +26,28 @@ public class Level6Initializer : MonoBehaviour, ILevelInitializer
 
 
         Dictionary<int, Building> hqDicts = new Dictionary<int, Building>();
-        hqDicts[1] = _hq1;
-        hqDicts[2] = _hq2;
-        hqDicts[3] = _hq3;
+        hqDicts[1] = hq1;
+        hqDicts[2] = hq2;
+        hqDicts[3] = hq3;
 
         AIManager ai1 = Instantiate(LevelManager.Instance.aiManagerPrefab).GetComponent<AIManager>();
 
         ai1.setPlayers(2);
         ai1.SetHQs(hqDicts);
-        ai1.SetGoals(new List<Building>() { monument, _hq1 });
-
-        ai1.AddBuilding(_hq1);
-        ai1.AddBuilding(_hq2);
-        ai1.AddBuilding(_hq3);
+        ai1.SetGoals(new List<Building>() { monument, hq1 });
+        ai1.AddBuilding(hq1);
+        ai1.AddBuilding(hq2);
+        ai1.AddBuilding(hq3);
         ai1.AddBuilding(monument);
 
         AIManager ai2 = Instantiate(LevelManager.Instance.aiManagerPrefab).GetComponent<AIManager>();
 
         ai2.setPlayers(3);
         ai2.SetHQs(hqDicts);
-        ai2.SetGoals(new List<Building>() { monument, _hq1 });
-
-        ai2.AddBuilding(_hq1);
-        ai2.AddBuilding(_hq2);
-        ai2.AddBuilding(_hq3);
+        ai2.SetGoals(new List<Building>() { monument, hq1 });
+        ai2.AddBuilding(hq1);
+        ai2.AddBuilding(hq2);
+        ai2.AddBuilding(hq3);
         ai2.AddBuilding(monument);
 
         ai1.StartPlaying();
@@ -60,9 +58,39 @@ public class Level6Initializer : MonoBehaviour, ILevelInitializer
     {
         if (building.BuildingInformation.Type != BuildingInformation.BuildingType.hq) return;
 
-        if (_hq1.Owner != 1 && _hq2.Owner != 1 && _hq3.Owner != 1)
+        if (oldOwner == 1)
+        {
+            _player1Hqs--;
+        }
+        else if (oldOwner == 2)
+        {
+            _player2Hqs--;
+        }
+        else if (oldOwner == 3)
+        {
+            _player3Hqs--;
+        }
+
+        if (newOwner == 1)
+        {
+            _player1Hqs++;
+        }
+        else if (newOwner == 2)
+        {
+            _player2Hqs++;
+        }
+        else if (newOwner == 3)
+        {
+            _player3Hqs++;
+        }
+
+        if (_player1Hqs == 0)
         {
             LevelManager.Instance.Defeat();
+        }
+        else if (_player1Hqs == 3)
+        {
+            LevelManager.Instance.Victory();
         }
     }
 
